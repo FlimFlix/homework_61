c933557437762a22c3de04edfa307f367d538e5c
Archer <arseniy.tarapon@gmail.com>
2019-02-28 19:08:59 +0600

    Импортировал все компнонеты в app.js и вывел их.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import './App.css';
+import AboutUs from "./containers/AboutUs/AboutUs";
+import Home from "./containers/Home/Home"
+import Layout from "./Layout";
+import Contacts from "./containers/Contacts/Contacts"
 
 class App extends Component {
-  render() {
-    return (
-      <div> Пустота </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/about" component={AboutUs}/>
+                        <Route path="/contacts" component={Contacts}/>
+                    </Switch>
+                </Layout>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
